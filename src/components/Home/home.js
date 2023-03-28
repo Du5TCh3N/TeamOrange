61d99b9c240ffceccaad6780e2876b0e08a144af
Yongqi-du <60711033+Yongqi-du@users.noreply.github.com>
2023-03-29 00:04:02 +0100

    changes

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -4,6 +4,35 @@ import { Layout, Space, Card, Col, Row,Modal } from 'antd';
 const Home = () => {
 
 
+  return (
+
+    
+        <Row gutter={[16,16]}>
+
+        <Col span={24}>
+          <Card title="Data Visualisation Platform for Social Housing " bordered={false}style={{backgroundColor: 'rgba(255,242,232, 0.0)', border: 0 }} headStyle={{backgroundColor: 'rgba(255, 255, 255, 0.4)', border: 0 }} bodyStyle={{backgroundColor: 'rgba(230,255,251,)', border: 0 }}>
+          </Card>
+        </Col>
+
+        <Col span={8}>
+          <Card  bordered={false}style={{backgroundColor: 'rgba(255,242,232, 0.0)', border: 0 }} headStyle={{backgroundColor: 'rgba(255, 255, 255, 0.4)', border: 0 }} bodyStyle={{backgroundColor: 'rgba(230,255,251,)', border: 0 }}>
+          </Card>
+        </Col>
+
+
+        <Col span={8}>
+          <Card title=" " bordered={false}style={{backgroundColor: 'rgba(255,242,232, 0.0)', border: 0 }} headStyle={{backgroundColor: 'rgba(255, 255, 255, 0.4)', border: 0 }} bodyStyle={{backgroundColor: 'rgba(230,255,251,)', border: 0 }}>
+          </Card>
+        </Col>
+
+
+        <Col span={8}>
+          <Card title=" " bordered={false}style={{backgroundColor: 'rgba(255,242,232, 0.0)', border: 0 }} headStyle={{backgroundColor: 'rgba(255, 255, 255, 0.4)', border: 0 }} bodyStyle={{backgroundColor: 'rgba(230,255,251,)', border: 0 }}>
+          </Card>
+        </Col>
+        </Row>
+
+  )
 }
 
 export default Home;
