d8ae782b42425f45056a393235f90c98b6704f49
Fahim Mohammed <mohammedf2606@gmail.com>
2023-03-31 05:00:13 +0100

    Add commenting app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
+// Import CSS files
 import 'antd/dist/reset.css';
- 
- 
-import{ HashRouter } from 'react-router-dom'
-
 import './App.css';
-import { BrowserRouter as Router, Route,Routes } from 'react-router-dom';
 
+// Import required modules
+import { BrowserRouter as Router} from 'react-router-dom';
 import BasicLayout from "./components/BasicLayout/basicLayout";
-
-// // Comment out the next 3 lines when editing locally
 import { Amplify } from 'aws-amplify';
-
-import { withAuthenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 
+// Import AWS Amplify configuration
 import awsExports from './aws-exports';
 Amplify.configure(awsExports);
 
-const apiEndpoint = 'https://4h1l85ptte.execute-api.eu-west-2.amazonaws.com/default/python-modeller'
-
+// Set API endpoint for AWS Amplify
+const apiEndpoint = 'https://4h1l85ptte.execute-api.eu-west-2.amazonaws.com/default/python-modeller';
 Amplify.API.configure({
   endpoints: [
     {
@@ -29,19 +24,17 @@ Amplify.API.configure({
   ],
 });
 
+// Define the main App component
 function App(){
-    
   return(
+    // Set up the router for the app
     <Router>
-    <div className = "App">
-    <BasicLayout />
- 
-    
-    </div>
+      <div className = "App">
+        <BasicLayout />
+      </div>
     </Router>
-
   )
 }
 
+// Export the App component as the default export
 export default App;
-// export default withAuthenticator(App);
